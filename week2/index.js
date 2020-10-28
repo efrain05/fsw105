95a2172d818401c8904cf43044b721bf6c808824
efrain05 <efrain.reyes@bryanuniversity.edu>
2020-10-27 20:12:42 -0400

    if_statement_olympics

---
+++ week2/index.js
@@ -0,0 +1,96 @@
+//Preliminaries
+
+if(5 > 3){
+    console.log("is greater than")
+}
+
+if(3 === 3){
+    console.log("is the length")
+}
+
+if(cat === dog){
+    console.log("print")
+}else {
+    console.log("not the same")
+}
+
+//Bronze Medal
+
+var name = "Bobby"
+var age = 12
+
+if(age > 18){
+    console.log("Bobby is allowed to go to the movie")
+}else{
+    console.log("Bobby is not allowed to go to the movie")
+}
+
+//Silver Medal
+
+if(1 === "1"){
+    console.log("strict")
+}else if (1 === "1"){
+    console.log("loose")
+    console.log("abstract")
+}else{
+    console.log("not equal at all")
+}
+
+if(1 <= 2 && 2 === 4){
+    console.log("yes")
+}
+
+//Gold Medal
+
+//1
+var myNum =10 
+
+if (myNum <= 20){
+    console.log(15)
+}
+
+//2
+var string = "dog"
+
+if (string === "dog"){
+    console.log("dog")
+}
+
+//3
+var num = 22 === 22
+console.log(num)
+
+//4
+
+
+
+//5
+var s = 24
+var num = 12
+
+if (s > num){
+    console.log(s)
+}
+
+//6
+var s = 57
+var n = 12
+var age =24
+var name = "John"
+var lastName= "doe"
+
+if (age <= s){
+    console.log(name + " " + lastName + " " + "is" + " " +age)
+}
+
+
+//7
+
+var myNum = 10;
+
+if(myNum % 2 === 0){
+    console.log("even")
+}else{
+    console.log("odd")
+}
+
