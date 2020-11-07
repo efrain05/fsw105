8f3c65490b77b45b19b500ad8fe8dbfec332881c
efrain05 <efrain.reyes@bryanuniversity.edu>
2020-11-06 23:45:52 -0500

    loops_and_arrays

--- week3/index.js
+++ week3/index.js
@@ -62,15 +62,16 @@ var peopleWhoWantToSeeMadMaxFuryRoad = [
 
 // 0-100 loop
 
+
 for(var i = 0; i < 101; i++){
-if([i] % 2 === 0 ){
- console.log([i] + " even")
-}else{
-    if([i] % 11 === 0){
-        console.log([i] + " odd")
+    if([i] % 2 === 0 ){
+     console.log([i] + " even")
+    }else{
+        {
+            console.log([i] + " odd")
+        }
     }
-}
-}
+    }
 
 // Extra Credit
 
